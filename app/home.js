1d7f8aeda1b921c27078d2e5ec950f6d9ff4d079
Shohaib hossain <khanshohaibhossain@gmail.com>
2025-11-17 13:30:43 +0600

    implemented the theme prodiver

--- app/home.js
+++ app/home.js
@@ -2,39 +2,48 @@ import { useEffect, useState } from "react";
 import { SafeAreaView, ScrollView, View } from "react-native";
 import { COLORS, SIZES } from "../constants/theme";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import ScreenHeaderBtn from "../../components/ScreenHeaderBtn";
-import Welcome from "../../components/Welcome";
-import DailyQuote from "../../components/DailyQuote";
-import PopularMeditation from "../../components/PopularMeditation";
-import DailyMeditation from "../../components/DailyMeditation";
-
+import ScreenHeaderBtn from "../components/ScreenHeaderBtn";
+import Welcome from "../components/Welcome";
+import PopularMeditation from "../components/PopularMeditation";
+import DailyMeditation from "../components/DailyMeditation";
+import { useTheme } from "../context/ThemeProvider";
 const Home = () => {
     const [userDetails, setUserDetails] = useState(null);
-    useEffect(() => {
-        loadUserDetails();
-    }, []);
-    const loadUserDetails = async () => {
-        const user = await AsyncStorage.getItem("userDetails");
-        console.log("user", user);
-        setUserDetails(user);
-    };
+  useEffect(() => {
+    loadUserDetails();
+  }, []);
+  const loadUserDetails = async () => {
+    const user = await AsyncStorage.getItem("userDetails");
+    console.log("user", user);
+    setUserDetails(user);
+  };
+  const { theme, toggleTheme } = useTheme();
+  console.log('theme',theme);
+  const isDarkMode = theme === "dark";
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-            <ScreenHeaderBtn/>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View
-                    style={{
-                        flex: 1,
-                        padding: SIZES.medium,
-                    }}
-                    testID="screensDisplay"
-                >
-                    <Welcome userDetails={userDetails ? JSON.parse(userDetails) : null} />
-                    <DailyQuote/>
-                    <PopularMeditation/><DailyMeditation/>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
+     <>
+       <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: isDarkMode ? COLORS.darkBackground : COLORS.lightWhite,
+      }}
+    >
+     <ScreenHeaderBtn/>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+          testID="screensDisplay"
+        >
+<Welcome userDetails={userDetails ? JSON.parse(userDetails) : null} isDarkMode={isDarkMode}/>
+<PopularMeditation isDarkMode={isDarkMode}/>
+<DailyMeditation isDarkMode={isDarkMode}/>
+</View>
+      </ScrollView>
+    </SafeAreaView>
+     </>
     );
   };
   export default Home;
--- constants/theme.js
+++ constants/theme.js
@@ -2,20 +2,19 @@ const COLORS = {
     primary: "#312651",
     secondary: "#444262",
     tertiary: "#FF7754",
-  
     gray: "#83829A",
     gray2: "#C1C0C8",
-  
     white: "#F3F4F8",
     lightWhite: "#FAFAFC",
+    darkBackground: "#000000",
+  lightText: "#000000",
+  darkText: "#FFFFFF",
   };
-  
   const FONT = {
     regular: "DMRegular",
     medium: "DMMedium",
     bold: "DMBold",
   };
-  
   const SIZES = {
     xSmall: 10,
     small: 12,
@@ -24,7 +23,6 @@ const COLORS = {
     xLarge: 24,
     xxLarge: 32,
   };
-  
   const SHADOWS = {
     small: {
       shadowColor: "#000",
@@ -47,6 +45,4 @@ const COLORS = {
       elevation: 5,
     },
   };
-  
   export { COLORS, FONT, SIZES, SHADOWS };
-  
